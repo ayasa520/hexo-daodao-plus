191aaab3b5b6a0cfe673146aa756dade083f1da3
rikka <1103670381@qq.com>
2021-07-21 02:01:53 +0800

    fix: 判断回调函数

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,7 +4,7 @@ function get_data(url, callback, extra) {
     xmlHttp.send(null);
     xmlHttp.onreadystatechange = function () {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            if (extra)
+            if (typeof extra === "function")
                 callback(JSON.parse(xmlHttp.responseText), extra);
             else
                 callback(JSON.parse(xmlHttp.responseText));
@@ -27,7 +27,7 @@ function daodao_page_init(url, template_html, extra) {
             sec.className = "timeline page-1";
             sec.innerHTML = html;
             document.getElementById("bber").appendChild(sec);
-            if(extra)
+            if(typeof extra === "function")
                 extra();
         }
         if (template !== 'function') {
